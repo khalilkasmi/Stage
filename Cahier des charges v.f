db928684a3135db0e0cef189661d3d55433519d6
khalil kasmi <khalilkasmi1997@gmail.com>
2019-03-27 15:59:06 +0000

    Create Cahier des charges v.f

---
+++ Cahier des charges v.f
@@ -0,0 +1,135 @@
+Cahier des charges
+
+1 - Contexte et définition du projet
+
+Le projet est un annuaire de recherche des agences de location des voitures dans tout le Maroc, qui va permettre aux gens de trouver des locations parmi plusieurs locations ayant été ajoutées par des entreprises qui peuvent être des personnes morales ou des personnes physiques.
+
+2 - Objectif du projet
+
+L’objectif premier du site web est d’offrir aux visiteurs la possibilité de chercher des locations, de trier les résultats des recherches afin de les amener à passer des réservations de voitures.
+Le site réservera un espace pour que les entreprises puissent gérer leurs agences et voitures.
+
+3 - Périmètre du projet
+
+-	Nous nous concentrerons sur les entreprises qui offrent leurs services au Maroc 
+-	Une entreprise de location doit être conforme aux critères du site afin de bénéficier des services du site web
+-	L’Age du conducteur doit être au moins 21 ans 
+
+4 - Description fonctionnelle des besoins
+
+-	Visiteur/client du site web 
+•	  Chercher une location en tenant compte des critères :
+    o	Lieu de restitution
+    o	Code promotionnel
+    o	Age du conducteur
+  •	Supprimer son compte
+  •	Consulter les résultats
+    o	Filtrer les résultats selon les critères :
+      	Types de voitures
+      	Sociétés de location
+      	Spécifications
+      	Lieu de prise en charge
+      	Capacité
+  •	Passer une réservation
+    o	Fournir des infos de paiement :
+      	Le numéro de carte de crédit
+      	Le cryptogramme
+      	Date de validité
+    o	Fournir des infos personnelles
+      	Nom
+      	Prénom
+      	Email
+      	Date de naissance 
+      	Téléphone
+    o	Eventuellement, un numéro de vol
+    o	Choisir une option de protection 
+  •	S’inscrire
+    	Nom
+    	Prénom
+    	Email
+    	Date de naissance 
+    	Téléphone
+    	Age
+    	Mot de passe
+  •	Se connecter
+    o	User Name/mail
+    o	Mot de passe
+  •	Se déconnecter 
+  •	Restaurer un mot de passe 
+    o	User Name/mail
+  •	Contacter le service client
+  •	Ajouter des avis sur une voiture/entreprise
+  •	Ajouter des plaints
+  •	Choisir une option de protection 
+  •	Gérer son profile
+  •	Valider une réservation
+-	Entreprise 
+  •  S’inscrire 
+    	Raison sociale
+    	Adresse
+    	Logo
+    	Téléphone / fax
+    	Ville
+    	Statut juridique
+    	Description
+    	Email
+    	Mot de passe
+    	Points
+    	Les dossiers de vérification (sous forme des fichier zip ou pdfs ou images)
+  •	A discuter // code patente 
+    o	Se connecter
+      	Email
+      	Mot de passe
+    o	Se déconnecter
+    o	Gérer les agences
+    	  Ajouter des agences
+      	Points
+      	Ville
+      	Entreprise
+      	Nombre total de voitures
+      	Nombre de voitures disponibles,
+      	Nombre de voitures louées
+      	L’adresse
+      	Email
+      	Téléphone
+    	 Supprimer une agence
+    	 Modifier le profil d’une agence
+  •	Gérer le parc des voitures
+    	 Ajouter des voitures
+      •	Matricule
+      •	Agence
+      •	Type
+      •	Marque
+      •	Nombre de places
+      •	Couleur
+      •	Description
+      •	Année
+      •	État
+      •	Assurance
+      •	Statut (loué, disponible, en phase d’entretien)
+      •	Points
+    	Modifier le statut d’une voiture
+    	Fournir des codes promotionnels 
+    	Supprimer des voitures
+  •	Gérer son profile
+  •	Fournir des factures
+
+-	Gérant
+  •	Se connecter 
+    	User Name
+    	Mot de passe
+  •	Gérer les entreprises
+    	Approuver une entreprise 
+    	Approuver une agence
+    	Supprimer une entreprise 
+  •	Gérer les comptes
+  •	Supprimer 
+  •	Approuver
+  •	Répondre a des plaints et réclamations
+
+-	Système
+  o	Envoyer des emails de confirmation des réservations
+  o	Envoyer des emails des nouveautés 
+  o	Vérifier les informations de paiement
+  o	Vérifier une entreprise (à discuter)
+  o	Offrir du support (bot) 
